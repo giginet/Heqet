4a0b7d5641778a5272ca3349ad095b91254ebd9e
giginet <giginet.net@gmail.com>
2012-01-15 07:17:26 +0900

    Add KWUtility

---
+++ Heqet/util/KWUtility.h
@@ -0,0 +1,15 @@
+//
+//  KWUtility.h
+//  _Kobold2D-With-Heqet-Template_
+//
+//  Created by  on 1/15/12.
+//  Copyright (c) 2012 Kawaz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KWUtility : NSObject
+
++ (NSString*)fileNameWithiPadSuffix:(NSString*)fileName;
+
+@end
---
+++ Heqet/util/KWUtility.m
@@ -0,0 +1,34 @@
+//
+//  KWUtility.m
+//  _Kobold2D-With-Heqet-Template_
+//
+//  Created by  on 1/15/12.
+//  Copyright (c) 2012 Kawaz. All rights reserved.
+//
+
+#import "KWUtility.h"
+
+@implementation KWUtility
+
+const NSString* IPAD_DEFAULT_SUFFIX = @"-ipad";
+
++ (NSString*)fileNameWithiPadSuffix:(NSString *)fileName {
+  if ([CCDirector sharedDirector].currentDeviceIsIPad) {
+    NSArray* path = [fileName componentsSeparatedByString:@"."];
+    NSString* basename = [path objectAtIndex:0];
+    NSString* ext = [path lastObject];
+    NSString* suffix = [KKConfig stringForKey:@"iPadSuffix"];
+    if (!suffix) {
+      suffix = (NSString*)IPAD_DEFAULT_SUFFIX;
+    }
+    NSString* suffixedPath = [NSString stringWithFormat:@"%@%@%@", basename, suffix, ext];
+    if (![[NSBundle mainBundle] pathForResource:[NSString stringWithFormat:@"%@%@", basename, suffix] ofType:ext] ) {
+      return suffixedPath;
+    } else {
+      CCLOG(@"The file named '%@' is not found.", suffixedPath);
+    }
+  }
+  return fileName;
+}
+
+@end
