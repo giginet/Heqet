93bf258682d5c6eb60d4dae7153a98a693f563e7
giginet <giginet.net@gmail.com>
2013-01-23 00:39:20 +0900

    Add cocos2d-extensions as submodule.

---
+++ Heqet/template/KWMovieLogoLayer.h
@@ -0,0 +1,13 @@
+//
+//  KWMovieLogoLayer.h
+//  MultipleSession
+//
+//  Created by giginet on 2013/1/23.
+//
+//
+
+#import "KWLayer.h"
+
+@interface KWMovieLogoLayer : KWLayer
+
+@end
---
+++ Heqet/template/KWMovieLogoLayer.m
@@ -0,0 +1,13 @@
+//
+//  KWMovieLogoLayer.m
+//  MultipleSession
+//
+//  Created by giginet on 2013/1/23.
+//
+//
+
+#import "KWMovieLogoLayer.h"
+
+@implementation KWMovieLogoLayer
+
+@end
